cec3bd8ee8c6fd158719672e2a5cd2ec13965a15
xinyueDang <xinyue.dang@algodepth.com>
2021-02-06 14:33:27 -0500

    Create user.service.client.js

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -6,9 +6,35 @@ function AdminUserServiceClient() {
     this.updateUser = updateUser;
     this.url = 'https://wbdv-generic-server.herokuapp.com/api/001054614/users';
     var self = this;
-    function createUser(user) {}
-    function findAllUsers() {}
+    function createUser(user) {
+        return fetch(self.url, {
+              method: 'POST',
+              headers: {
+                'content-type': 'application/json'
+              },
+              body: JSON.stringify(user)
+            }).then(function (response) {
+              return response.json()
+            })
+    }
+    function findAllUsers() {
+        return fetch(self.url)
+              .then(function (response) {
+                return response.json()
+            })
+    }
     function findUserById(userId) {}
-    function updateUser(userId, user) {}
-    function deleteUser(userId) {}
-}
+    function updateUser(userId, user) {
+        return fetch(`${self.url}/${userId}`, {
+              method: 'PUT',
+              headers: {
+                'content-type': 'application/json'
+              },
+              body: JSON.stringify(user)
+            }).then(response => response.json())
+    }
+    function deleteUser(userId) {
+        return fetch(`${self.url}/${userId}`,
+              {method: 'DELETE'})
+          }
+ }
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -18,12 +18,12 @@
         updateBtn = $(".wbdv-update")
         updateBtn.click(updateUser)
         tbody = $(".wbdv-tbody")
-//
-//          courseService.findAllCourses()
-//            .then(function (actualCoursesFromServer) {
-//              courses = actualCoursesFromServer
-//              renderCourses(courses)
-//            })
+
+        userService.findAllUsers()
+          .then(function (actualUsersFromServer) {
+            users = actualUsersFromServer
+            renderUsers(users)
+          })
     }
     var users = [];
     function createUser() {
@@ -34,41 +34,30 @@
         user.lastName = lastNameFld.val();
         user.role = roleFld.val();
 
-        /*userService.createUser(user)
+        userService.createUser(user)
             .then(function (actualUser) {
               users.push(actualUser)
               renderUsers(users)
-            })*/
-        users.push(user)
-        renderUsers(users)
+        })
     }
 
     function deleteUser(event) {
-        console.log(event)
         var deleteBtn = jQuery(event.target)
         var theClass = deleteBtn.attr("class")
         var theIndex = deleteBtn.attr("id")
         var theId = users[theIndex]._id
-//        courseService.deleteCourse(theId)
-//          .then(function (status) {
-//           courses.splice(theIndex, 1)
-//           renderCourses(courses)
-//           })
-        users.splice(theIndex, 1)
-        renderUsers(users)
+        userService.deleteUser(theId)
+          .then(function (status) {
+               users.splice(theIndex, 1)
+               renderUsers(users)
+           })
     }
 
     var selectedUser = null
     function selectUser(event) {
-        console.log(event)
-
         var selectBtn = $(event.target)
-        var theClass = selectBtn.attr("class")
-        var theIndex = selectBtn.attr("id")
-        var theId = users[theIndex]._id
-        //var theId = selectBtn.attr("id")
+        var theId = selectBtn.attr("id")
         selectedUser = users.find(user => user._id === theId)
-        console.log(selectedUser)
         usernameFld.val(selectedUser.username);
         firstNameFld.val(selectedUser.firstName);
         lastNameFld.val(selectedUser.lastName);
@@ -82,15 +71,13 @@
         selectedUser.lastName = lastNameFld.val();
         selectedUser.role = roleFld.val();
 
-        /*userService.updateCourse(selectedCourse._id, selectedCourse)
+        userService.updateUser(selectedUser._id, selectedUser)
           .then(function (status) {
-            var index = courses.findIndex(course => course._id === selectedCourse._id)
-            courses[index] = selectedCourse
-            renderCourses(courses)
-          })*/
-        var index = users.findIndex(user => user._id === selectedUser._id)
-        users[index] = selectedUser
-        renderUsers(users)
+            var index = users.findIndex(user => user._id === user._id)
+            users[index] = selectedUser
+            renderUsers(users)
+          })
+
     }
 
     function renderUsers(users) {
@@ -109,7 +96,7 @@
                     <span class="pull-right">
                        <i class="fa-lg fa fa-times wbdv-remove" id="${i}"></i>
                           &nbsp;
-                       <i class="fa-lg fa fa-pencil wbdv-edit" id="${i}"></i>
+                       <i class="fa-lg fa fa-pencil wbdv-edit" id="${user._id}"></i>
                     </span>
                 </td>
             </tr>
